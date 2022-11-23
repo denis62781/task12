86d1b3438e1ac13fa1052811c9b9706e176ab450
tellepat <49366661+tellepat@users.noreply.github.com>
2022-11-23 20:33:10 +0300

    1

---
+++ src/main/java/Item.java
@@ -0,0 +1,64 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table(
+        name = "items"
+)
+public class Item extends Thread {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    @Column(
+            name = "id_item"
+    )
+    private int id;
+    @Column(
+            name = "values"
+    )
+    private int values;
+    @Version
+    long version;
+
+    public Item() {
+    }
+
+    public Item(int values) {
+        this.values = values;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId(int id) {
+        return id;
+    }
+
+    public int getValues() {
+        return this.values;
+    }
+
+    public void setValues(int values) {
+        this.values = values;
+    }
+
+    public long getVersion() {
+        return this.version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+}
